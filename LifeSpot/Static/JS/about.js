71de8eecdccee8375e45b396876864defef17568
pavlosius <pavlosius@mail.ru>
2023-12-05 18:43:02 +0300

    Задание 30.3.6

--- LifeSpot/Static/JS/about.js
+++ LifeSpot/Static/JS/about.js
@@ -26,26 +26,33 @@ function getReview() {
     writeReview(review)
 }
 
-
-
-function getComment() {
-    // Создаем объект обычного комментария
-    let comment = {}
-
+function Comment() {
     // Запросим имя
-    comment.author = prompt("Как вас зовут ?")
-    if (comment.author == null) {
+    this.author = prompt("Как вас зовут ?")
+    if (this.author == null) {
+        this.empty = true
         return
     }
 
     // Запросим текст
-    comment.text = prompt("Оставьте отзыв")
-    if (comment.text == null) {
+    this.text = prompt("Оставьте отзыв")
+    if (this.text == null) {
+        this.empty = true
         return
     }
 
     // Сохраним текущее время
-    comment.date = new Date().toLocaleString()
+    this.date = new Date().toLocaleString()
+}
+
+function addComment() {
+    // Создаем объект обычного комментария
+    let comment = new Comment()
+
+    // проверяем, успешно ли юзер осуществил ввод
+    if (comment.empty) {
+        return;
+    }
 
     // Запросим,, хочет ли пользователь оставить полноценный отзыв или это будет обычный комментарий
     let enableLikes = confirm('Разрешить пользователям оценивать ваш отзыв?')
